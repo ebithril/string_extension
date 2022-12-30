dda33021fcc5884df76c68f0922411812a6e78ab
Patrik Görling <p.gorling@gmail.com>
2022-12-31 00:38:16 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+trait StringExtension {
+    fn find_all(&self, in_char: char) -> Vec<usize>;
+}
+
+impl StringExtension for String {
+    fn find_all(&self, in_char: char) -> Vec<usize> {
+        let mut result: Vec<usize> = Vec::new();
+        for (i, c) in self.chars().enumerate() {
+            if c == in_char {
+                result.push(i);
+            }
+        }
+
+        result
+    }
+}
+
+impl StringExtension for str {
+    fn find_all(&self, in_char: char) -> Vec<usize> {
+        let mut result: Vec<usize> = Vec::new();
+        for (i, c) in self.chars().enumerate() {
+            if c == in_char {
+                result.push(i);
+            }
+        }
+
+        result
+    }
+}
